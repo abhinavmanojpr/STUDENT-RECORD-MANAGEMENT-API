2090bf2d5d04acbca025b13d8baa85769c4227b5
Abhinav M <abhinavmanojpr@gmail.com>
2026-08-16 16:44:59 +0530

    Initial Commit

---
+++ src/test/java/com/studentmanagement/StudentRecordAPI/StudentRecordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentmanagement.StudentRecordAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRecordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
